04fca186b3d8a560e37aae469e9be57fb87e6d9d
jubairahmed22 <jubairahmed060@gmail.com>
2024-03-24 00:04:52 +0600

    added specialist page

--- src/app/(withCommonLayout)/page.tsx
+++ src/app/(withCommonLayout)/page.tsx
@@ -1,10 +1,12 @@
 import HeroSection from "@/components/UI/HomePage/HeroSection/HeroSection";
+import Specialist from "@/components/UI/HomePage/Specialist/Specialist";
 
 const HomePage =  () => {
 
   return (
     <div className="h-screen">
       <HeroSection></HeroSection>
+      <Specialist></Specialist>
     </div>
   );
 };
