7a6ea3030d9038590e5fd655871c0c4a27348d49
jubairahmed22 <jubairahmed060@gmail.com>
2024-03-25 00:19:54 +0600

    icon added

--- src/components/UI/HomePage/WhyUs/WhyUs.tsx
+++ src/components/UI/HomePage/WhyUs/WhyUs.tsx
@@ -1,6 +1,6 @@
 import { Box, Container, Grid, Typography } from "@mui/material";
 import assets from "@/assets";
-import chooseUsImg from "@/assets/choose-us.png"
+import chooseUsImg from "@/assets/choose-us.png";
 import Image from "next/image";
 
 const servicesData = [
@@ -48,10 +48,24 @@ const WhyUs = () => {
           </Typography>
         </Box>
         <Grid container spacing={2}>
-            <Grid item md={6}></Grid>
-            <Grid item md={6}>
-                <Image src={chooseUsImg} alt="chooseUsPage"></Image>
-            </Grid>
+          <Grid item md={6}>
+            <Box>
+              <Box>
+                <Image src={servicesData[0].imageSrc} width={80} alt="award" />
+              </Box>
+              <Box>
+                <Typography variant="h6" component="h6" fontWeight={600}>
+                  {servicesData[0].title}
+                </Typography>
+                <Typography variant="h6" color="primary body1" fontWeight={300}>
+                  {servicesData[0].description}
+                </Typography>
+              </Box>
+            </Box>
+          </Grid>
+          <Grid item md={6}>
+            <Image src={chooseUsImg} alt="chooseUsPage"></Image>
+          </Grid>
         </Grid>
       </Box>
     </Container>
