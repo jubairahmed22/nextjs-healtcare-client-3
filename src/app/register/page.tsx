fe5479b8fef339c657ae64ac1ac1cc239ffaf97a
jubairahmed22 <jubairahmed060@gmail.com>
2024-03-27 00:18:04 +0600

    register page running

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,8 +1,24 @@
+import { Box, Container, Stack } from "@mui/material";
 
 const registerPage = () => {
     return (
         <div>
-            register here
+            <Container>
+                <Stack sx={{
+                    justifyContent: 'center',
+                    alignItems: 'center'
+                }}>
+                <Box sx={{
+                    maxWidth: 600,
+                    width: "100%",
+                    boxShadow: 1,
+                    borderRadius: 1,
+                    p: 4,
+                }}>
+
+                </Box>
+                </Stack>
+            </Container>
         </div>
     );
 };
