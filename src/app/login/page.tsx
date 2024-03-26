93b686e9a00fd6dd478a632cda83c7315d572c5f
jubairahmed22 <jubairahmed060@gmail.com>
2024-03-27 00:54:18 +0600

    login page start

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,9 +1,46 @@
+import { Box, Container, Stack, Typography } from "@mui/material";
+import Image from "next/image";
+import assets from "@/assets";
 
 const LoginPage = () => {
     return (
-        <div>
-            <h1>This is  login main page</h1>
-        </div>
+        <Container>
+            <Stack sx={{
+                height: "100vh",
+                justifyContent: 'center',
+                alignItems: "center"
+            }}>
+             <Box
+              sx={{
+                maxWidth: 600,
+                width: "100%",
+                boxShadow: 1,
+                borderRadius: 1,
+                p: 4,
+                textAlign: "center"
+              }}
+             >
+<Stack
+              sx={{
+                justifyContent: "center",
+                alignItems: "center",
+              }}
+            >
+              <Image
+                src={assets.svgs.logo}
+                width={50}
+                height={50}
+                alt="logo"
+              ></Image>
+              <Box>
+                <Typography variant="h6" fontWeight={600}>
+                  Patient Register
+                </Typography>
+              </Box>
+            </Stack>
+             </Box>
+            </Stack>
+        </Container>
     );
 };
 
