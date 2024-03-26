14415d1e8ee43304f650dc1870b38a526e7abb2f
jubairahmed22 <jubairahmed060@gmail.com>
2024-03-27 00:59:10 +0600

    just email and pass field added

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,55 +1,58 @@
-import { Box, Button, Container, Grid, Stack, TextField, Typography } from "@mui/material";
+import {
+  Box,
+  Button,
+  Container,
+  Grid,
+  Stack,
+  TextField,
+  Typography,
+} from "@mui/material";
 import Image from "next/image";
 import assets from "@/assets";
 import Link from "next/link";
 
 const LoginPage = () => {
-    return (
-        <Container>
-            <Stack sx={{
-                height: "100vh",
-                justifyContent: 'center',
-                alignItems: "center"
-            }}>
-             <Box
-              sx={{
-                maxWidth: 600,
-                width: "100%",
-                boxShadow: 1,
-                borderRadius: 1,
-                p: 4,
-                textAlign: "center"
-              }}
-             >
-<Stack
-              sx={{
-                justifyContent: "center",
-                alignItems: "center",
-              }}
-            >
-              <Image
-                src={assets.svgs.logo}
-                width={50}
-                height={50}
-                alt="logo"
-              ></Image>
-              <Box>
-                <Typography variant="h6" fontWeight={600}>
-                  Patient Register
-                </Typography>
-              </Box>
-            </Stack>
+  return (
+    <Container>
+      <Stack
+        sx={{
+          height: "100vh",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Box
+          sx={{
+            maxWidth: 600,
+            width: "100%",
+            boxShadow: 1,
+            borderRadius: 1,
+            p: 4,
+            textAlign: "center",
+          }}
+        >
+          <Stack
+            sx={{
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            <Image
+              src={assets.svgs.logo}
+              width={50}
+              height={50}
+              alt="logo"
+            ></Image>
             <Box>
-              <form>
+              <Typography variant="h6" fontWeight={600}>
+                Patient Register
+              </Typography>
+            </Box>
+          </Stack>
+          <Box>
+            <form>
               <Grid container spacing={2} my={1}>
-                <Grid item md={12}>
-                  <TextField
-                    label="Name"
-                    variant="outlined"
-                    size="small"
-                    fullWidth={true}
-                  />
-                </Grid>
+               
                 <Grid item md={6}>
                   <TextField
                     label="Email"
@@ -68,39 +71,25 @@ const LoginPage = () => {
                     fullWidth={true}
                   />
                 </Grid>
-                <Grid item md={6}>
-                  <TextField
-                    label="Contact Number"
-                    type="tel"
-                    variant="outlined"
-                    size="small"
-                    fullWidth={true}
-                  />
-                </Grid>
-                <Grid item md={6}>
-                  <TextField
-                    label="Address"
-                    type="text"
-                    variant="outlined"
-                    size="small"
-                    fullWidth={true}
-                  />
-                </Grid>
+               
               </Grid>
-              </form>
-              <Button sx={{
-                margin: "10px 0px"
-              }} fullWidth={true}>
-                Register
-              </Button>
-              <Typography component="p" fontWeight={300}>
-                Do you already have an account? <Link href="/login">Login</Link>
-               </Typography>
-            </Box>
-             </Box>
-            </Stack>
-        </Container>
-    );
+            </form>
+            <Button
+              sx={{
+                margin: "10px 0px",
+              }}
+              fullWidth={true}
+            >
+              Login
+            </Button>
+            <Typography component="p" fontWeight={300}>
+              Don&apos;t have an account? <Link href="/register">Create an account</Link>
+            </Typography>
+          </Box>
+        </Box>
+      </Stack>
+    </Container>
+  );
 };
 
 export default LoginPage;
