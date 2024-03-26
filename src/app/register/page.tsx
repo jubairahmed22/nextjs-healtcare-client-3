235daf2d76b9e9a00a612edd5e448b38850bf112
jubairahmed22 <jubairahmed060@gmail.com>
2024-03-27 00:22:54 +0600

    images added

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,9 +1,13 @@
 import { Box, Container, Stack } from "@mui/material";
+import assets from "@/assets";
+import Image from "next/image";
 
 const registerPage = () => {
     return (
         <div>
-            <Container>
+            <Container sx={{
+                padding: "50px"
+            }}>
                 <Stack sx={{
                     justifyContent: 'center',
                     alignItems: 'center'
@@ -15,7 +19,14 @@ const registerPage = () => {
                     borderRadius: 1,
                     p: 4,
                 }}>
-
+                 <Stack
+                   sx={{
+                    justifyContent: "center",
+                    alignItems: "center",
+                   }}
+                   >
+                  <Image src={assets.svgs.logo} width={50} height={50} alt="logo"></Image>
+                 </Stack>
                 </Box>
                 </Stack>
             </Container>
