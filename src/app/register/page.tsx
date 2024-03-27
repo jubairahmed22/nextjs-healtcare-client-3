e9078405beb1c6aa339e22115324702b006afed4
jubairahmed22 <jubairahmed060@gmail.com>
2024-03-28 00:55:02 +0600

    soemthing changes

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -119,7 +119,7 @@ const registerPage = () => {
 
                   />
                 </Grid>
-                <Grid item md={6}>
+                <Grid item md={4}>
                   <TextField
                     label="Contact Number"
                     type="tel"
