e3cdd3fb6bc0f52adbde51953886dd437bf54389
jubairahmed22 <jubairahmed060@gmail.com>
2024-04-01 01:03:07 +0600

    solve some error solving

--- src/utils/local-storage.ts
+++ src/utils/local-storage.ts
@@ -1,5 +1,5 @@
 export const setToLocalStorage = (key:string, token:string) => {
-   if(!key || typeof window === "undefied"){
+   if(!key || typeof window === "undefined"){
     return "";
    }
  return localStorage.setItem(key, token);
