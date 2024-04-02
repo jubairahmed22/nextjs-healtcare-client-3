bd0877c88e460039306c3e77356ef37528168224
jubairahmed22 <jubairahmed060@gmail.com>
2024-04-03 00:33:17 +0600

    phForm page added

---
+++ src/components/Forms/PHForm.tsx
@@ -0,0 +1,13 @@
+import { useForm } from "react-hook-form";
+
+const PHForm = () => {
+    const methods = useForm()
+
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default PHForm;
