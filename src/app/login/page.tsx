9a9115dd8d5f0e793ff11e708d539a186ccc4834
jubairahmed22 <jubairahmed060@gmail.com>
2024-04-04 00:54:00 +0600

    add reset in phform

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -74,7 +74,7 @@ const LoginPage = () => {
             ></Image>
             <Box>
               <Typography variant="h6" fontWeight={600}>
-                Patient
+                Patient Login
               </Typography>
             </Box>
           </Stack>
