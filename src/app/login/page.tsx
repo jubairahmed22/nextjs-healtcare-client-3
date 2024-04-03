ecaf91ad9e221923725801a16cc6a9214854bd4f
jubairahmed22 <jubairahmed060@gmail.com>
2024-04-04 00:51:34 +0600

    add reset in phform

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -70,7 +70,7 @@ const LoginPage = () => {
               src={assets.svgs.logo}
               width={50}
               height={50}
-              alt="logo"
+              alt="log"
             ></Image>
             <Box>
               <Typography variant="h6" fontWeight={600}>
