8404a9f04f6be46ad3665d22e2df9e77a6f3ca5d
jubairahmed22 <jubairahmed060@gmail.com>
2024-04-05 06:55:34 +0600

    something changes

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -73,7 +73,7 @@ const LoginPage = () => {
               alt="logo"
             ></Image>
             <Box>
-              <Typography variant="h6" fontWeight={900}>
+              <Typography variant="h6" fontWeight={600}>
                 Patient Login
               </Typography>
             </Box>
