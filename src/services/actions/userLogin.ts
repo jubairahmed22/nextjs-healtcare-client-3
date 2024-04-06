8ca614b6f9e0ca2180baa10eaf9be0534a6a7cc2
jubairahmed22 <jubairahmed060@gmail.com>
2024-04-07 00:32:39 +0600

    add resolver

--- src/services/actions/userLogin.ts
+++ src/services/actions/userLogin.ts
@@ -1,6 +1,6 @@
 "use server"
 
-import { FieldValues } from "react-hook-for";
+import { FieldValues } from "react-hook-form";
 
 export const userLogin = async (data: FieldValues) => {
     const res = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_API_URL}/auth/login`, {
