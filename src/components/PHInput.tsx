3532995520bc5cf4ae7054b4f83c998cbf3876d9
jubairahmed22 <jubairahmed060@gmail.com>
2024-04-07 00:34:51 +0600

    add resolver

--- src/components/PHInput.tsx
+++ src/components/PHInput.tsx
@@ -26,7 +26,7 @@ const PHInput = ({name,sx, required,label, type = "text", size="small", fullWidt
           sx={{ ...sx }}
           label={label}
           type={type}
-          variant="outlined"
+          variant="outline"
           size={size}
           fullWidth={fullWidth}
           placeholder={label}
