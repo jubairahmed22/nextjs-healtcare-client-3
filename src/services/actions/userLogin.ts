e11aa1a1472ac4f5b93798100edfdb9be2021978
jubairahmed22 <jubairahmed060@gmail.com>
2024-04-07 00:33:19 +0600

    add resolver

--- src/services/actions/userLogin.ts
+++ src/services/actions/userLogin.ts
@@ -3,7 +3,7 @@
 import { FieldValues } from "react-hook-form";
 
 export const userLogin = async (data: FieldValues) => {
-    const res = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_API_URL}/auth/`, {
+    const res = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_API_URL}/auth/login`, {
         method : "POST",
         headers : {
             "Content-Type" : "application/json"
