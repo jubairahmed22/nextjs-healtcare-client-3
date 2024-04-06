5106174e248afce4a3678fdd6b348a35f0b348a9
jubairahmed22 <jubairahmed060@gmail.com>
2024-04-07 00:34:16 +0600

    add resolver

--- src/services/actions/userLogin.ts
+++ src/services/actions/userLogin.ts
@@ -6,7 +6,7 @@ export const userLogin = async (data: FieldValues) => {
     const res = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_API_URL}/auth/login`, {
         method : "POST",
         headers : {
-            "Content-Type" : "application"
+            "Content-Type" : "application/json"
         },
         body: JSON.stringify(data),
         cache: "no-store"
