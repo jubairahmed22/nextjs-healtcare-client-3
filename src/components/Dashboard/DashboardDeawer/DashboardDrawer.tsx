40333855d626323a80d34380f62a1923330044a9
jubairahmed22 <jubairahmed060@gmail.com>
2024-04-09 17:01:57 +0600

    add sidebar

--- src/components/Dashboard/DashboardDeawer/DashboardDrawer.tsx
+++ src/components/Dashboard/DashboardDeawer/DashboardDrawer.tsx
@@ -1,22 +1,14 @@
-"use client"
-
+"use client";
 import * as React from 'react';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import CssBaseline from '@mui/material/CssBaseline';
-import Divider from '@mui/material/Divider';
 import Drawer from '@mui/material/Drawer';
 import IconButton from '@mui/material/IconButton';
-import InboxIcon from '@mui/icons-material/MoveToInbox';
-import List from '@mui/material/List';
-import ListItem from '@mui/material/ListItem';
-import ListItemButton from '@mui/material/ListItemButton';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import ListItemText from '@mui/material/ListItemText';
-import MailIcon from '@mui/icons-material/Mail';
 import MenuIcon from '@mui/icons-material/Menu';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
+import SideBar from '../SideBar/SideBar';
 
 const drawerWidth = 240;
 
@@ -40,37 +32,7 @@ export default function DashboardDrawer({children} : {children: React.ReactNode}
     }
   };
 
-  const drawer = (
-    <div>
-      <Toolbar />
-      <Divider />
-      <List>
-        {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
-          <ListItem key={text} disablePadding>
-            <ListItemButton>
-              <ListItemIcon>
-                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-              </ListItemIcon>
-              <ListItemText primary={text} />
-            </ListItemButton>
-          </ListItem>
-        ))}
-      </List>
-      <Divider />
-      <List>
-        {['All mail', 'Trash', 'Spam'].map((text, index) => (
-          <ListItem key={text} disablePadding>
-            <ListItemButton>
-              <ListItemIcon>
-                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-              </ListItemIcon>
-              <ListItemText primary={text} />
-            </ListItemButton>
-          </ListItem>
-        ))}
-      </List>
-    </div>
-  );
+ 
 
   return (
     <Box sx={{ display: 'flex' }}>
@@ -116,7 +78,7 @@ export default function DashboardDrawer({children} : {children: React.ReactNode}
             '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
           }}
         >
-          {drawer}
+        <SideBar></SideBar>
         </Drawer>
         <Drawer
           variant="permanent"
@@ -126,7 +88,7 @@ export default function DashboardDrawer({children} : {children: React.ReactNode}
           }}
           open
         >
-          {drawer}
+        <SideBar></SideBar>
         </Drawer>
       </Box>
       <Box
