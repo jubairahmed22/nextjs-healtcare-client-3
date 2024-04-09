0ac7822453bf4fcc79bc8602358cfc0ccbc38208
jubairahmed22 <jubairahmed060@gmail.com>
2024-04-09 14:44:01 +0600

    add base api

--- src/lib/Providers/Providers.tsx
+++ src/lib/Providers/Providers.tsx
@@ -2,12 +2,16 @@
 
 import { ThemeProvider } from "@mui/material";
 import { theme } from "../theme/theme";
+import { Provider } from "react-redux";
+import { store } from "@/redux/store";
 
 const Providers = ({children}: {children:React.ReactNode}) => {
     return (
+        <Provider store={store}>
         <ThemeProvider theme={theme}>
             {children}
         </ThemeProvider>
+        </Provider>
     );
 };
 
