6eb7db82a6dcbad4823449e7526f8bc08fe0edde
jubairahmed22 <jubairahmed060@gmail.com>
2024-04-09 16:28:47 +0600

    dashboard layout

---
+++ src/app/(withDashboardLayout)/layout.tsx
@@ -0,0 +1,11 @@
+
+const DashboardLayout = ({children} : {children: React.ReactNode}) => {
+    return (
+        <div>
+            <h1>dashboard</h1>
+            {children}
+        </div>
+    );
+};
+
+export default DashboardLayout;
