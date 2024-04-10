4838b30346d187922a3771114ad21b36aeaf84f9
jubairahmed22 <jubairahmed060@gmail.com>
2024-04-10 17:41:26 +0600

    role

--- src/components/Dashboard/SideBar/SideBar.tsx
+++ src/components/Dashboard/SideBar/SideBar.tsx
@@ -23,7 +23,7 @@ const SideBar = () => {
             <Stack 
             sx={{
                 py:1,
-                mt:1
+                mt:6
             }}
             direction="row"
             justifyContent="center"
