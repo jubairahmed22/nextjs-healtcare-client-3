b31d51e7751e500eee76ffe3308ae8b94db331c0
jubairahmed22 <jubairahmed060@gmail.com>
2024-04-10 16:31:28 +0600

    add side data

--- src/components/Dashboard/SideBar/SideBar.tsx
+++ src/components/Dashboard/SideBar/SideBar.tsx
@@ -4,19 +4,21 @@ import InboxIcon from '@mui/icons-material/MoveToInbox';
 import Image from "next/image";
 import assets from '@/assets';
 import Link from "next/link";
+import { drawerItems } from "@/utils/drawerItems";
+import { UserRole } from "@/types";
 
 const SideBar = () => {
     const drawer = (
         <div>
             <List>
-            {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
-              <ListItem key={text} disablePadding>
+            {drawerItems("admin" as UserRole).map((item, index) => (
+              <ListItem key={index} disablePadding>
                 <ListItemButton>
                   <ListItemIcon>
                     {index % 2 === 0 ? <InboxIcon
                      /> : <MailIcon/>}
                   </ListItemIcon>
-                  <ListItemText primary={text} />
+                  <ListItemText primary={item.title} />
                 </ListItemButton>
               </ListItem>
             ))}
