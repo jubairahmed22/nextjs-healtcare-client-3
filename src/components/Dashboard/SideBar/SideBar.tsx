75ed4bdfb8dd029c0ae9f31d9552c0b2483e6234
jubairahmed22 <jubairahmed060@gmail.com>
2024-04-10 17:34:54 +0600

    role

--- src/components/Dashboard/SideBar/SideBar.tsx
+++ src/components/Dashboard/SideBar/SideBar.tsx
@@ -5,8 +5,18 @@ import Link from "next/link";
 import { drawerItems } from "@/utils/drawerItems";
 import { UserRole } from "@/types";
 import SidebarItem from "./SidebarItem";
+import { getUserInfo } from "@/services/auth.services";
+import { useEffect, useState } from "react";
 
 const SideBar = () => {
+    
+    const [userRole, setUserRole] = useState("");
+
+    useEffect(() => {
+      const {role} =  getUserInfo();
+      setUserRole(role)
+    }, [])
+
     
     return (
         <Box>
@@ -30,7 +40,7 @@ const SideBar = () => {
             </Typography>
             </Stack>
             <List>
-            {drawerItems("admin" as UserRole).map((item, index) => (
+            {drawerItems(userRole as UserRole).map((item, index) => (
               <SidebarItem key={index}  item={item}></SidebarItem>
             ))}
           </List>
