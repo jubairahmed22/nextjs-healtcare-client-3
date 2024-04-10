87f2c1ecf68e389a7e401a7c396f4571e223b94d
jubairahmed22 <jubairahmed060@gmail.com>
2024-04-10 16:39:54 +0600

    working with side bar

--- src/components/Dashboard/SideBar/SideBar.tsx
+++ src/components/Dashboard/SideBar/SideBar.tsx
@@ -1,30 +1,13 @@
 import { Box, Divider, List, ListItem, ListItemButton, ListItemIcon, ListItemText, Stack, Toolbar, Typography } from "@mui/material";
-import MailIcon from '@mui/icons-material/Mail';
-import InboxIcon from '@mui/icons-material/MoveToInbox';
 import Image from "next/image";
 import assets from '@/assets';
 import Link from "next/link";
 import { drawerItems } from "@/utils/drawerItems";
 import { UserRole } from "@/types";
+import SidebarItem from "./SidebarItem";
 
 const SideBar = () => {
-    const drawer = (
-        <div>
-            <List>
-            {drawerItems("admin" as UserRole).map((item, index) => (
-              <ListItem key={index} disablePadding>
-                <ListItemButton>
-                  <ListItemIcon>
-                    {index % 2 === 0 ? <InboxIcon
-                     /> : <MailIcon/>}
-                  </ListItemIcon>
-                  <ListItemText primary={item.title} />
-                </ListItemButton>
-              </ListItem>
-            ))}
-          </List>
-         </div>
-      );
+    
     return (
         <Box>
             <Stack 
@@ -46,7 +29,11 @@ const SideBar = () => {
                 PH Health Care
             </Typography>
             </Stack>
-            {drawer}
+            <List>
+            {drawerItems("admin" as UserRole).map((item, index) => (
+              <SidebarItem key={index} index={index} item={item}></SidebarItem>
+            ))}
+          </List>
         </Box>
     );
 };
