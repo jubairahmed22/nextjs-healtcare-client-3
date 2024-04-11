fb913a75ffe736e131cf5d5d885713c972cf60a5
jubairahmed22 <jubairahmed060@gmail.com>
2024-04-12 01:18:41 +0600

    add axiosInstance

--- src/helpers/axios/axiosBaseQuery.ts
+++ src/helpers/axios/axiosBaseQuery.ts
@@ -3,6 +3,7 @@ import { createApi } from '@reduxjs/toolkit/query'
 import type { BaseQueryFn } from '@reduxjs/toolkit/query'
 import axios from 'axios'
 import type { AxiosRequestConfig, AxiosError } from 'axios'
+import { instance as axiosInstance } from './axiosInstance'
 
 export const axiosBaseQuery =
   (
@@ -22,7 +23,7 @@ export const axiosBaseQuery =
   > =>
   async ({ url, method, data, params, headers, contentType }) => {
     try {
-      const result = await axios({
+      const result = await axiosInstance({
         url: baseUrl + url,
         method,
         data,
