cc6fe192ee767085b11072c3efef838800336035
jubairahmed22 <jubairahmed060@gmail.com>
2024-04-12 01:22:42 +0600

    add axiosInstance

--- src/helpers/axios/axiosInstance.ts
+++ src/helpers/axios/axiosInstance.ts
@@ -1,3 +1,5 @@
+import { authKay } from "@/contants/authkey";
+import { getFromLocalStorage } from "@/utils/local-storage";
 import axios from "axios";
 
 
@@ -6,5 +8,35 @@ instance.defaults.headers.post["Content-Type"] = "application/json";
 instance.defaults.headers["Accepts"] = "application/json";
 instance.defaults.timeout = 60000; 
 
+// Add a request interceptor
+instance.interceptors.request.use(
+    function (config) {
+    // Do something before request is sent
+    const accessToken  = getFromLocalStorage(authKay);
+    
+    if(accessToken){
+        config.headers.Authorization = accessToken
+    }
+    return config;
+  }, function (error) {
+    // Do something with request error
+    return Promise.reject(error);
+  });
+
+// Add a response interceptor
+instance.interceptors.response.use(
+    function (response) {
+    // Any status code that lie within the range of 2xx cause this function to trigger
+    // Do something with response data
+    const responseObject = {
+        data: response?.data?.data,
+        meta: response?.data?.meta
+    }
+    return response;
+  }, function (error) {
+    // Any status codes that falls outside the range of 2xx cause this function to trigger
+    // Do something with response error
+    return Promise.reject(error);
+  });
 
 export {instance}
