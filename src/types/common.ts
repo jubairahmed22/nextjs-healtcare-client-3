2bee0d5ea68eed03b9911ffdb4b77acbe03ec4cc
jubairahmed22 <jubairahmed060@gmail.com>
2024-04-12 01:33:50 +0600

    add axiosInstance

--- src/types/common.ts
+++ src/types/common.ts
@@ -21,10 +21,14 @@ export type ResponseSuccessType = {
     data: any;
     meta?: IMeta;
 }
-// export type IGenericErrorResponse = {
-//     statusCode: number;
-//     message: string;
-//     errorMessages: IGenericErrorMessage[];
-// }
+export type IGenericErrorResponse = {
+    statusCode: number;
+    message: string;
+    errorMessages: IGenericErrorMessage[];
+}
+export type IGenericErrorMessage = {
+    path: string | number;
+    message: string;
+}
 
 export type UserRole = keyof typeof USER_ROLE;
