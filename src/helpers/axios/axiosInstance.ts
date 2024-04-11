a432980d57a626c87d601412bb307fd3134c1188
jubairahmed22 <jubairahmed060@gmail.com>
2024-04-12 01:10:11 +0600

    add axiosInstance

--- src/helpers/axios/axiosInstance.ts
+++ src/helpers/axios/axiosInstance.ts
@@ -3,7 +3,8 @@ import axios from "axios";
 
 const instance = axios.create();
 instance.defaults.headers.post["Content-Type"] = "application/json";
- 
+instance.defaults.headers["Accepts"] = "application/json";
+instance.defaults.timeout = 60000; 
 
 
 export {instance}
