9573ce91fad0537e410f685f82d48caf823f3a11
jubairahmed22 <jubairahmed060@gmail.com>
2024-04-13 13:05:05 +0600

    add type ph file uploader

--- src/components/Forms/PHFileUploader.tsx
+++ src/components/Forms/PHFileUploader.tsx
@@ -3,18 +3,9 @@ import { SxProps, styled } from '@mui/material/styles';
 import Button from '@mui/material/Button';
 import CloudUploadIcon from '@mui/icons-material/CloudUpload';
 import { Controller, useFormContext } from 'react-hook-form';
+import { Input } from '@mui/material';
 
-const VisuallyHiddenInput = styled('input')({
-  clip: 'rect(0 0 0 0)',
-  clipPath: 'inset(50%)',
-  height: 1,
-  overflow: 'hidden',
-  position: 'absolute',
-  bottom: 0,
-  left: 0,
-  whiteSpace: 'nowrap',
-  width: 1,
-});
+
 
 type TProps = {
   name: string,
@@ -28,7 +19,7 @@ export default function PHFileUploader({name, label, sx}: TProps) {
       <Controller 
       name={name}
       control={control} 
-      render={()=>{
+      render={({field: {onChange, value, ...field}})=>{
         return (
           <Button
           component="label"
@@ -39,7 +30,10 @@ export default function PHFileUploader({name, label, sx}: TProps) {
           sx={{...sx}}
         >
           {label || "Upload file"}
-          <VisuallyHiddenInput type="file" />
+          <Input {...field} type={name} value={value?.fileName}
+          onChange={e=> onChange((e?.target as HTMLInputElement).files?.[0])}
+          style={{display:"none"}}
+          ></Input>
         </Button>
         )
       }}/>
