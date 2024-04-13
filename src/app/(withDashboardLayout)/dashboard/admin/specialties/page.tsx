0b051f153c2cfcdbf722b6388b96440b1591f9db
jubairahmed22 <jubairahmed060@gmail.com>
2024-04-13 22:45:06 +0600

    add table specialtis

--- src/app/(withDashboardLayout)/dashboard/admin/specialties/page.tsx
+++ src/app/(withDashboardLayout)/dashboard/admin/specialties/page.tsx
@@ -18,7 +18,7 @@ const SpecialtiesPage = () => {
     width: 100,
     renderCell:({row})=> {
     return <Box>
-        <Image src={row.icon} alt="ico"
+        <Image src={row.icon} alt="icon"
          width={40} height={40}></Image>
     </Box>
     } },
