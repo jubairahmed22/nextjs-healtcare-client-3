c2871dcc1bba1c6d9008dfe8099930d2fa8e54e0
jubairahmed22 <jubairahmed060@gmail.com>
2024-04-18 00:22:05 +0600

    wallet page complete

--- src/components/Dashboard/DashboardDeawer/DashboardDrawer.tsx
+++ src/components/Dashboard/DashboardDeawer/DashboardDrawer.tsx
@@ -62,7 +62,7 @@ export default function DashboardDrawer({children} : {children: React.ReactNode}
             Hi, Jubair Ahmed
           </Typography>
           <Typography variant="body2" noWrap component="div" color="primary.main">
-            Welcome to, PH 
+            Welcome to, PH Health Care!
           </Typography>
           </Box>
         </Toolbar>
