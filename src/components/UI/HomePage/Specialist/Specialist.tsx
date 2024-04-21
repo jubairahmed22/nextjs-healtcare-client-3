7f7eb4b1f7d78bb4a9aabcda5e2eff0e583910c4
jubairahmed22 <jubairahmed060@gmail.com>
2024-04-21 23:39:13 +0600

    add date picker

--- src/components/UI/HomePage/Specialist/Specialist.tsx
+++ src/components/UI/HomePage/Specialist/Specialist.tsx
@@ -43,7 +43,7 @@ const Specialist = async () => {
                 padding: "40px 10px",
                 "& img": {
                   width: "50px",
-                  height: "50px",
+                  height: "5078px",
                   margin: "0 auto",
                 },
                 "&:hover": {
