b2e81620392f325c73bbceeab207def2f042c596
jubairahmed22 <jubairahmed060@gmail.com>
2024-04-26 23:22:51 +0600

    games card complete

--- src/components/UI/HomePage/Specialist/Specialist.tsx
+++ src/components/UI/HomePage/Specialist/Specialist.tsx
@@ -14,7 +14,7 @@ const Specialist = async () => {
     <Container>
       <Box
         sx={{
-          margin: "80px 0114px",
+          margin: "80px 0px",
           textAlign: "center",
         }}
       >
