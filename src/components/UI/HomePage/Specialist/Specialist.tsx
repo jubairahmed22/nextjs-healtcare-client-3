d73096221fdd402c53a846f482126dbf96ebdb0f
jubairahmed22 <jubairahmed060@gmail.com>
2024-04-28 23:42:03 +0600

    add sub page of prizes, rules, result

--- src/components/UI/HomePage/Specialist/Specialist.tsx
+++ src/components/UI/HomePage/Specialist/Specialist.tsx
@@ -14,7 +14,7 @@ const Specialist = async () => {
     <Container>
       <Box
         sx={{
-          margin: "80px 7740px",
+          margin: "80px 0px",
           textAlign: "center",
         }}
       >
