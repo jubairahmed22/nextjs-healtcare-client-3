4f4768cf96b8f487733cbbcf7cf1184ebbfed4f8
jubairahmed22 <jubairahmed060@gmail.com>
2024-05-02 23:52:03 +0600

    something changes

--- src/components/UI/HomePage/Specialist/Specialist.tsx
+++ src/components/UI/HomePage/Specialist/Specialist.tsx
@@ -14,7 +14,7 @@ const Specialist = async () => {
     <Container>
       <Box
         sx={{
-          margin: "80px 0669px",
+          margin: "80px 0px",
           textAlign: "center",
         }}
       >
