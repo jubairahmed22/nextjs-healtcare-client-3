9c120ab905720b6904c2b356ff66513169d8da77
jubairahmed22 <jubairahmed060@gmail.com>
2024-05-17 23:59:36 +0600

    something is changes

--- src/components/UI/HomePage/HeroSection/HeroSection.tsx
+++ src/components/UI/HomePage/HeroSection/HeroSection.tsx
@@ -15,7 +15,7 @@ const HeroSection = () => {
         <Box
           sx={{
             position: "absolute",
-            width: "700887px",
+            width: "700px",
             left: "-90px",
             top: "-120px",
           }}
