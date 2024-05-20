d3f7703cf6a3540a23652e785b7b46985c53e87c
jubairahmed22 <jubairahmed060@gmail.com>
2024-05-20 23:51:25 +0600

    add refreash token

--- src/helpers/axios/axiosInstance.ts
+++ src/helpers/axios/axiosInstance.ts
@@ -1,51 +1,66 @@
 import { authKay } from "@/contants/authkey";
+import { getNewAccessToken } from "@/services/auth.services";
 import { IGenericErrorResponse, ResponseSuccessType } from "@/types";
-import { getFromLocalStorage } from "@/utils/local-storage";
-import { formatProdErrorMessage } from "@reduxjs/toolkit";
+import { getFromLocalStorage, setToLocalStorage } from "@/utils/local-storage";
 import axios from "axios";
-
 
 const instance = axios.create();
 instance.defaults.headers.post["Content-Type"] = "application/json";
-instance.defaults.headers["Accepts"] = "application/json";
-instance.defaults.timeout = 60000; 
+instance.defaults.headers["Accept"] = "application/json";
+instance.defaults.timeout = 60000;
 
 // Add a request interceptor
 instance.interceptors.request.use(
-    function (config) {
+  function (config) {
     // Do something before request is sent
-    const accessToken  = getFromLocalStorage(authKay);
-    
-    if(accessToken){
-        config.headers.Authorization = accessToken
+    const accessToken = getFromLocalStorage(authKay);
+
+    if (accessToken) {
+      config.headers.Authorization = accessToken;
     }
     return config;
-  }, function (error) {
+  },
+  function (error) {
     // Do something with request error
     return Promise.reject(error);
-  });
+  }
+);
 
 // Add a response interceptor
 instance.interceptors.response.use(
-    //@ts-ignore
-    function (response) {
+  //@ts-ignore
+  function (response) {
     // Any status code that lie within the range of 2xx cause this function to trigger
     // Do something with response data
     const responseObject: ResponseSuccessType = {
-        data: response?.data?.data,
-        meta: response?.data?.meta
-    }
+      data: response?.data?.data,
+      meta: response?.data?.meta,
+    };
     return responseObject;
-  }, function (error) {
+  },
+  async function (error) {
     // Any status codes that falls outside the range of 2xx cause this function to trigger
     // Do something with response error
-    const responseObject: IGenericErrorResponse = {
-       statusCode: error?.response?.data?.statusCode || 500,
-       message: error?.response?.data?.message || "Something went wrong!!",
-       errorMessages: error?.response?.data?.message
-    };
-    // return Promise.reject(error);
-    return responseObject;
-  });
+    // console.log(error);
+    const config = error.config;
+    // console.log(config);
+    if (error?.response?.status === 500 && !config.sent) {
+      config.sent = true;
+      const response = await getNewAccessToken();
+      const accessToken = response?.data?.accessToken;
+      config.headers["Authorization"] = accessToken;
+      setToLocalStorage(authKay, accessToken);
+      return instance(config);
+    } else {
+      const responseObject: IGenericErrorResponse = {
+        statusCode: error?.response?.data?.statusCode || 500,
+        message: error?.response?.data?.message || "Something went wrong!!!",
+        errorMessages: error?.response?.data?.message,
+      };
+      // return Promise.reject(error);
+      return responseObject;
+    }
+  }
+);
 
-export {instance}
+export { instance };
--- src/services/auth.services.ts
+++ src/services/auth.services.ts
@@ -1,30 +1,46 @@
-import { authKay } from "@/contants/authkey"
+import { authKay } from "@/contants/authkey";
+import { instance as axiosInstance } from "@/helpers/axios/axiosInstance";
 import { decodedToken } from "@/utils/jwt";
-import { getFromLocalStorage, removeFromLocalStorage, setToLocalStorage } from "@/utils/local-storage"
 
-export const storeUserInfo = ({accessToken}: {accessToken: string}) => {
-   return setToLocalStorage(authKay, accessToken)
+import {
+  getFromLocalStorage,
+  removeFromLocalStorage,
+  setToLocalStorage,
+} from "@/utils/local-storage";
+
+export const storeUserInfo = ({ accessToken }: { accessToken: string }) => {
+  //   console.log(accessToken);
+  return setToLocalStorage(authKay, accessToken);
 };
 
 export const getUserInfo = () => {
-   const authToken = getFromLocalStorage(authKay);
-   // console.log(authToken);
-   if(authToken) {
-      const decodedData:any = decodedToken(authToken);
-      return {
-         ...decodedData,
-         role: decodedData?.role.toLowerCase()
-      }
-   }
+  const authToken = getFromLocalStorage(authKay);
+  //   console.log(authToken);
+  if (authToken) {
+    const decodedData: any = decodedToken(authToken);
+    return {
+      ...decodedData,
+      role: decodedData?.role.toLowerCase(),
+    };
+  }
 };
 
 export const isLoggedIn = () => {
-   const authToken = getFromLocalStorage(authKay);
-   if(authToken){
-      return !! authToken;
-   }
+  const authToken = getFromLocalStorage(authKay);
+  if (authToken) {
+    return !!authToken;
+  }
 };
 
 export const removeUser = () => {
-   return removeFromLocalStorage(authKay)
-}
+  return removeFromLocalStorage(authKay);
+};
+
+export const getNewAccessToken = async () => {
+  return await axiosInstance({
+    url: "http://localhost:5000/api/v1/auth/refresh-token",
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    withCredentials: true,
+  });
+};
