04bb603267b47c0238d8487a1ddfd4a95c1d6687
jubairahmed22 <jubairahmed060@gmail.com>
2024-05-24 23:47:19 +0600

    something changes

--- src/app/(withDashboardLayout)/layout.tsx
+++ src/app/(withDashboardLayout)/layout.tsx
@@ -1,6 +1,14 @@
+"use client";
 import DashboardDrawer from "@/components/Dashboard/DashboardDeawer/DashboardDrawer";
+import { isLoggedIn } from "@/services/auth.services";
+import { useRouter } from "next/navigation";
 
-const DashboardLayout = ({children} : {children: React.ReactNode}) => {
-    return <DashboardDrawer>{children}</DashboardDrawer>};
+const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
+  const router = useRouter();
+  if (!isLoggedIn()) {
+    return router.push("/login");
+  }
+  return <DashboardDrawer>{children} </DashboardDrawer>;
+};
 
 export default DashboardLayout;
